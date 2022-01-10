71d996b62c8ac268f9837088f73b61dbf1427e91
java <74448030+thinkcodegravity@users.noreply.github.com>
2022-01-09 19:01:13 -0600

    jquery javascript

--- src/main/java/com/example/demo/Jeenov22Application.java
+++ src/main/java/com/example/demo/Jeenov22Application.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Jeenov22Application {
 
 	public static void main(String[] args) {
+		System.out.println(" i am printing from server side");
 		SpringApplication.run(Jeenov22Application.class, args);
+		
 	}
 
 }
